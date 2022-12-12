c97a09b8c3a0fff47a2796d218620a0d28f66746
mueseb <mueseb18@htl-kaindorf.at>
2022-12-12 23:36:24 +0100

    optimized models

---
+++ src/main/java/com/mueseb/carpettrapdoors/block/CarpetTrapdoor.java
@@ -0,0 +1,36 @@
+package com.mueseb.carpettrapdoors.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.TrapDoorBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Half;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class CarpetTrapdoor extends TrapDoorBlock {
+
+    public CarpetTrapdoor(Properties p_i48307_1_) {
+        super(p_i48307_1_);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState p_57563_, BlockGetter p_57564_, BlockPos p_57565_, CollisionContext p_57566_) {
+        if (!p_57563_.getValue(OPEN)) {
+            return p_57563_.getValue(HALF) == Half.TOP ? Block.box(0.0, 15.0, 0.0, 16.0, 16.0, 16.0) : Block.box(0.0, 0.0, 0.0, 16.0, 1.0, 16.0);
+        } else {
+            switch(p_57563_.getValue(FACING)) {
+                case NORTH:
+                default:
+                    return Block.box(0.0, 0.0, 15.0, 16.0, 16.0, 16.0);
+                case SOUTH:
+                    return Block.box(0.0, 0.0, 0.0, 16.0, 16.0, 1.0);
+                case WEST:
+                    return Block.box(15.0, 0.0, 0.0, 16.0, 16.0, 16.0);
+                case EAST:
+                    return Block.box(0.0, 0.0, 0.0, 1.0, 16.0, 16.0);
+            }
+        }
+    }
+}
